213ca8f35e40ac0fc4b943467630228e008a6234
Pablo Valverde <Pablo.ValverdeVasile@alu.frt.utn.edu.ar>
2026-05-02 16:56:26 -0300

    Problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int valorCopi= originalValue;
+            valorCopi++;
+            Product productoCopiado = product;
+
+
+        }
+    }
+}
