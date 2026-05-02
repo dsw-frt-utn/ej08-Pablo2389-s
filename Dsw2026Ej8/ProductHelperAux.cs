07ee2e1f2f8b74d938315d830caa4259cdc999bc
Pablo Valverde <Pablo.ValverdeVasile@alu.frt.utn.edu.ar>
2026-05-02 16:34:31 -0300

    Problema 1

---
+++ Dsw2026Ej8/ProductHelperAux.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+
+        public partial class ProductHelper
+        {
+            // Acá definimos la constante para que el otro archivo la pueda usar
+            private const string FormatoEtiqueta = "[{0}] {1} - {2}";
+        }
+}
