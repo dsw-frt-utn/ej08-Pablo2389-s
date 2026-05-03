298f8459b54cd22d8d3a2cff9016ee69f053982f
Pablo Valverde <Pablo.ValverdeVasile@alu.frt.utn.edu.ar>
2026-05-03 16:33:16 -0300

    Problema 5

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalcularTotal();
+        }
+
+        public class Sale
+        {
+            public decimal _amount;
+            public decimal Amount 
+            {
+                get { return _amount; }
+                set { _amount = value; }
+            }
+            public Sale(decimal amount)
+            {
+                _amount = amount;
+            }
+
+            public virtual decimal CalcularTotal() 
+            {
+                return Amount;
+            }
+
+
+        }
+        public class RetailSale : Sale
+        {
+            public RetailSale(decimal amount) : base(amount){ }
+            public override decimal CalcularTotal()
+            {
+                return Amount;
+            }
+        }
+
+        public class WholesaleSale : Sale
+        {
+            public WholesaleSale(decimal amount) : base(amount ) { }
+
+            public override decimal CalcularTotal()
+            {
+                return Amount * 0.9m; // Aplicamos un descuento del 10%
+            }
+        } 
+    }
+}
